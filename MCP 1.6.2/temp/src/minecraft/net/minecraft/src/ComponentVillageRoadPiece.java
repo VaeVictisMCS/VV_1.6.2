234bf44e75a75dc255c0fed26c6ba23493685716
Nicolas Denisi <nicolas.denisi@hotmail.fr>
2013-08-20 19:56:06 +0200

    5.0 Ouden
    Starting project

---
+++ MCP 1.6.2/temp/src/minecraft/net/minecraft/src/ComponentVillageRoadPiece.java
@@ -0,0 +1,11 @@
+package net.minecraft.src;
+
+import net.minecraft.src.ComponentVillage;
+import net.minecraft.src.ComponentVillageStartPiece;
+
+public abstract class ComponentVillageRoadPiece extends ComponentVillage {
+
+   protected ComponentVillageRoadPiece(ComponentVillageStartPiece p_i2108_1_, int p_i2108_2_) {
+      super(p_i2108_1_, p_i2108_2_);
+   }
+}
